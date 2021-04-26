0937e2995a0e1cf1bb9a7fe7e39694886fe3cc1b
Halvor Blindheim <71223339+studenthalvor@users.noreply.github.com>
2021-04-26 19:51:21 +0200

    Siste

---
+++ src/Game.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable{
+
+    public Game() {
+
+    }
+
+    public void run() {}
+
+    public static void main(String[] args) {
+        new Game();
+    }
+}
